bdc9a9d701d9bd4bb85709e2ecf1575e7f5526c3
Jan Starke <jan.starke@t-systems.com>
2023-03-08 23:27:56 +0100

    added dos

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ipgrep
 search for IP addresses in text files
+
+# Usage
+
+```
+Usage: ipgrep [OPTIONS] [FILE]...
+
+Arguments:
+  [FILE]...  
+
+Options:
+  -i, --include <INCLUDE>        display only lines who match ALL of the specified criteria. Values
+                                 are delimited with comma [possible values: ipv4, ipv6, public,
+                                 private, loopback]
+  -x, --exclude <EXCLUDE>        hide lines who match ANY of the specified criteria. Values are
+                                 delimited with comma [possible values: ipv4, ipv6, public, private,
+                                 loopback]
+  -I, --ignore-ips <IGNORE_IPS>  ignore any of the specified IP addresses. Values are delimited with
+                                 comma
+  -c, --colors                   highlight interesting content using colors
+  -v, --verbose...               More output per occurrence
+  -q, --quiet...                 Less output per occurrence
+  -h, --help                     Print help
+  -V, --version                  Print version
+```
+
+License: GPL-3.0
--- tests/test_format_ipv4.rs
+++ tests/test_format_ipv4.rs
@@ -1,36 +1,95 @@
+use colored::Colorize;
 use ipgrep::{format_ipv4, IpFilter};
 
 #[test]
 fn test_loopback1() {
-    assert!(format_ipv4(&vec![][..], &vec![IpFilter::Loopback][..], &vec![][..], "127.0.0.1").is_some());
+    assert!(format_ipv4(
+        &vec![][..],
+        &vec![IpFilter::Loopback][..],
+        &vec![][..],
+        "127.0.0.1"
+    )
+    .is_some());
 }
 
 #[test]
 fn test_loopback2() {
-    assert_eq!(format_ipv4(&vec![][..], &vec![IpFilter::Loopback][..], &vec![][..], "abc 127.0.0.1"), Some("abc 127.0.0.1".to_string()));
+    #[allow(clippy::to_string_in_format_args)]
+    let expected = format!("abc {}", "127.0.0.1".bright_purple().to_string());
+    assert_eq!(
+        format_ipv4(
+            &vec![][..],
+            &vec![IpFilter::Loopback][..],
+            &vec![][..],
+            "abc 127.0.0.1"
+        ),
+        Some(expected)
+    );
 }
 
 #[test]
 fn test_loopback3() {
-    assert_eq!(format_ipv4(&vec![][..], &vec![IpFilter::Loopback][..], &vec![][..], "abc 127.0.0.1 def"), Some("abc 127.0.0.1 def".to_string()));
+    #[allow(clippy::to_string_in_format_args)]
+    let expected = format!("abc {} def", "127.0.0.1".bright_purple().to_string());
+    assert_eq!(
+        format_ipv4(
+            &vec![][..],
+            &vec![IpFilter::Loopback][..],
+            &vec![][..],
+            "abc 127.0.0.1 def"
+        ),
+        Some(expected)
+    );
 }
 
 #[test]
 fn test_loopback4() {
-    assert!(format_ipv4(&vec![][..], &vec![IpFilter::Loopback][..], &vec![][..], "hello, world").is_none());
+    assert!(format_ipv4(
+        &vec![][..],
+        &vec![IpFilter::Loopback][..],
+        &vec![][..],
+        "hello, world"
+    )
+    .is_none());
 }
 
 #[test]
 fn test_loopback5() {
-    assert!(format_ipv4(&vec![][..], &vec![IpFilter::Loopback][..], &vec![][..], "192.168.0.1").is_none());
+    assert!(format_ipv4(
+        &vec![][..],
+        &vec![IpFilter::Loopback][..],
+        &vec![][..],
+        "192.168.0.1"
+    )
+    .is_none());
 }
 
 #[test]
 fn test_loopback6() {
-    assert_eq!(format_ipv4(&vec![][..], &vec![IpFilter::Loopback][..], &vec![][..], "abc 127.0.0.1 def 192.168.0.1"), Some("abc 127.0.0.1 def 192.168.0.1".to_string()));
+    #[allow(clippy::to_string_in_format_args)]
+    let expected = format!(
+        "abc {} def {}",
+        "127.0.0.1".bright_purple().to_string(),
+        "192.168.0.1".bright_purple().to_string()
+    );
+    assert_eq!(
+        format_ipv4(
+            &vec![][..],
+            &vec![IpFilter::Loopback][..],
+            &vec![][..],
+            "abc 127.0.0.1 def 192.168.0.1"
+        ),
+        Some(expected)
+    );
 }
 
 #[test]
 fn test_loopback7() {
-    assert!(format_ipv4(&vec![IpFilter::Private][..], &vec![IpFilter::Loopback][..], &vec![][..], "abc 127.0.0.1 def 192.168.0.1").is_none());
+    assert!(format_ipv4(
+        &vec![IpFilter::Private][..],
+        &vec![IpFilter::Loopback][..],
+        &vec![][..],
+        "abc 127.0.0.1 def 192.168.0.1"
+    )
+    .is_none());
 }
